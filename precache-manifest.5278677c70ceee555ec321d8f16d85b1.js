40955947e97f2f2b52bc9f18233d3ff6f3837085
vkochev <vkochev@skbkontur.ru>
2020-06-02 20:15:11 +0500

    test build

---
+++ precache-manifest.5278677c70ceee555ec321d8f16d85b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ff7e860ea9ad13e1334c749b2421c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8843f8cfc7e26e484710",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "888da87d8ccabfadec51",
+    "url": "/static/js/2.42da07fe.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.42da07fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8843f8cfc7e26e484710",
+    "url": "/static/js/main.6988ec2a.chunk.js"
+  },
+  {
+    "revision": "3f7255c8d917c6b66273",
+    "url": "/static/js/runtime-main.36027638.js"
+  }
+]);
---
+++ precache-manifest.784062ed5ccf0168b6ff17daeeabca60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3932150722367342aa9ff46c132f043",
+    "url": "/abaci/index.html"
+  },
+  {
+    "revision": "6c7f73f84218b24e2f1e",
+    "url": "/abaci/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2508563859f530b1b903",
+    "url": "/abaci/static/js/2.2fd9d128.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/abaci/static/js/2.2fd9d128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c7f73f84218b24e2f1e",
+    "url": "/abaci/static/js/main.4c2a7550.chunk.js"
+  },
+  {
+    "revision": "3e74ca3f545a59346e52",
+    "url": "/abaci/static/js/runtime-main.a1d5662f.js"
+  }
+]);
